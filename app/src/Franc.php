ba7b56cdf9e87c7352c864c69d22cef22f02c017
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-07-30 22:33:25 +0900

    p.78 Refactoring

--- app/src/Franc.php
+++ app/src/Franc.php
@@ -4,7 +4,5 @@ declare(strict_types=1);
 namespace Money;
 
 class Franc extends Money {
-  public function __construct($amount, $currency) {
-    parent::__construct($amount, $currency);
-  }
+  
 }
