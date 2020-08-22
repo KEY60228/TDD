2f82911b73a54f2372499841c4a8c058e42be03f
Kenta Yamaguchi <Mt.mouth60228@gmail.com>
2020-08-22 22:27:41 +0900

    p.94 GREEN

--- app/src/Bank.php
+++ app/src/Bank.php
@@ -4,6 +4,9 @@ namespace Money;
 
 class Bank {
   public function reduce(Expression $source, String $to): Money {
+    if ($source instanceof Money) {
+      return $source;
+    }
     $sum = $source;
     return $sum->reduce($to);
   }
